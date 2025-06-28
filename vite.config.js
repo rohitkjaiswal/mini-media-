b7b64eaded69b90961260768b38277a62ac3c25b
Rohitkjaiswal <rohitkjaiswal243@gmail.com>
2025-06-28 11:01:46 +0530

    Fix : corrected vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: "/mini-media-/", // ✅ VERY IMPORTANT
+    base: './', // ✅ Use relative path for Netlify
     server: {
         watch: {
             usePolling: true,
